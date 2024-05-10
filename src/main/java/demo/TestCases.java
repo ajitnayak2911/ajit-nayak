218b7014d8c69cf7938d657491a43c7f239e25e6
ajit <ajitnayak007@gmail.com>
2024-05-10 15:18:51 +0530

    Google Form Automation code changes

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -13,29 +13,27 @@ import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class TestCases {
     ChromeDriver driver;
-    public TestCases()
-    {
+
+    public TestCases() {
         System.out.println("Constructor: TestCases");
         WebDriverManager.chromedriver().timeout(30).setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-    public void endTest()
-    {
+    public void endTest() {
         System.out.println("End Test: TestCases");
         driver.close();
         driver.quit();
 
     }
 
-    
-    public  void testCase01() throws InterruptedException{
+    public void testCase01() throws InterruptedException {
         System.out.println("Start Test case: testCase01");
-        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        driver.get(
+                "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
 
         // Implicitly wait for elements to be found
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -43,12 +41,9 @@ public class TestCases {
         // Fill out the form fields
         WebElement nameField = driver.findElement(By.xpath("(//input[@tabindex='0'])[1]"));
         nameField.click();
-        nameField.sendKeys("Ajit Nayak");
+        nameField.sendKeys("TEST");
 
         Thread.sleep(3000);
-
-
-        
 
         // Get the current epoch time in milliseconds
         long currentTimeMillis = System.currentTimeMillis();
@@ -62,7 +57,7 @@ public class TestCases {
         Thread.sleep(5000);
 
         WebElement selectexpradiobutton = driver.findElement(By.xpath("(//div[@class='vd3tt'])[2]"));
-        
+
         if (!selectexpradiobutton.isSelected()) {
             // Select the radio button
             selectexpradiobutton.click();
@@ -72,44 +67,45 @@ public class TestCases {
 
         // Find the checkbox element and select Java,Selenium,TestNG
 
-        WebElement checkbox1 =driver.findElement(By.xpath("//span[text()='Java']"));
+        WebElement checkbox1 = driver.findElement(By.xpath("//span[text()='Java']"));
         checkbox1.click();
         Thread.sleep(2000);
-        WebElement checkbox2=driver.findElement(By.xpath("//span[text()='Selenium']"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//span[text()='Selenium']"));
         checkbox2.click();
         Thread.sleep(3000);
-        WebElement checkbox4=driver.findElement(By.xpath("//span[text()='TestNG']"));
+        WebElement checkbox4 = driver.findElement(By.xpath("//span[text()='TestNG']"));
         checkbox4.click();
-        // List<WebElement> checkboxes = driver.findElements(By.xpath("//div[@class='eBFwI']"));
+        // List<WebElement> checkboxes =
+        // driver.findElements(By.xpath("//div[@class='eBFwI']"));
 
         // // Define the texts to match as per the selection to be done
         // String[] targetTexts = {"Java", "Selenium", "TestNG"};
 
-        
         // int count = 0;
         // for (WebElement checkbox : checkboxes) {
-        //     if (checkboxes.contains(targetTexts)) {
-        //         // Select the checkbox
-        //         checkbox.click();
-        //         count++;
-        //         break; 
-        //     }
+        // if (checkboxes.contains(targetTexts)) {
+        // // Select the checkbox
+        // checkbox.click();
+        // count++;
+        // break;
+        // }
         // }
 
         // Break the loop if you have selected all checkboxes
         // if (count == targetTexts.length) {
-        //     break;
-        
+        // break;
+
         Thread.sleep(5000);
-    
-           // Locate the dropdown element for "How should you be addressed?" and select the desired value
+
+        // Locate the dropdown element for "How should you be addressed?" and select the
+        // desired value
         WebElement dropdownElement = driver.findElement(By.xpath("//span[text()='Choose']"));
         dropdownElement.click();
 
         Thread.sleep(5000);
 
         dropdownElement.getText();
-        WebElement selectoption=driver.findElement(By.xpath("(//span[text()='Mr'])[2]"));
+        WebElement selectoption = driver.findElement(By.xpath("(//span[text()='Mr'])[2]"));
         selectoption.click();
 
         Thread.sleep(5000);
@@ -133,43 +129,51 @@ public class TestCases {
         // Enter the date 7 days ago into the date input field
         dateField.sendKeys(dateFormatted);
 
-       Thread.sleep(10000);
+        Thread.sleep(10000);
 
-
-
-       // Get the current time
+        // Get the current time
         LocalTime currentTime = LocalTime.now();
 
         // Format the time as "hh", "mm", and "a" (with AM/PM)
         DateTimeFormatter hourMinuteFormatter = DateTimeFormatter.ofPattern("hh");
-        //DateTimeFormatter amPmFormatter = DateTimeFormatter.ofPattern("a");
+        // DateTimeFormatter amPmFormatter = DateTimeFormatter.ofPattern("a");
         String hour = currentTime.format(hourMinuteFormatter);
         String minute = currentTime.format(DateTimeFormatter.ofPattern("mm"));
-        //String amPm = currentTime.format(amPmFormatter);
+        // String amPm = currentTime.format(amPmFormatter);
 
         // Locate and enter the hour input field
         WebElement hourField = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
-        //hourField.clear();
+        // hourField.clear();
         hourField.sendKeys(hour);
 
         // Locate and enter the minute input field
         WebElement minuteField = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
-        //minuteField.clear();
+        // minuteField.clear();
         minuteField.sendKeys(minute);
 
         // Locate and click the AM/PM dropdown
-        // WebElement amPmDropdown = driver.findElement(By.xpath("(//div[@class='e2CuFe eU809d'])[2]"));
+        // WebElement amPmDropdown = driver.findElement(By.xpath("(//div[@class='e2CuFe
+        // eU809d'])[2]"));
         // amPmDropdown.click();
 
         // Locate and select the AM/PM option
-        //WebElement amPmOption = driver.findElement(By.xpath("//div[@class='OA0qNb ncFHed QXL7Te']//span[text()='" + amPm + "']"));
+        // WebElement amPmOption = driver.findElement(By.xpath("//div[@class='OA0qNb
+        // ncFHed QXL7Te']//span[text()='" + amPm + "']"));
 
-           
-        //amPmOption.click();
+        // amPmOption.click();
 
         Thread.sleep(5000);
-        
+
+        WebElement SubmitButton = driver.findElement(By.xpath("//span[text()='Submit']"));
+        SubmitButton.click();
+
+        Thread.sleep(5000);
+
+        WebElement Capturemessage = driver.findElement(By.xpath("//div[@class='vHW8K']"));
+        String messageText = Capturemessage.getText();
+        if (messageText.contains("Thanks for your response, Automation Wizard!")) {
+            System.out.println("Success message: " + messageText);
+        }
         System.out.println("end Test case: testCase01");
     }
-    }   
-
+}
